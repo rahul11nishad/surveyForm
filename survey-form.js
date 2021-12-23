5b798566f23d5af4d801b432ae29a9549b313896
rahul11nishad <rahul004700@gmail.com>
2021-12-23 16:37:04 +0530

    we are adding new function

--- survey-form.js
+++ survey-form.js
@@ -5,3 +5,10 @@ function Animal(name,numLegs,fly,color)
     this.fly=false;
     this.color=color;
 }
+function Phone(brand,model,color,price)
+{
+    this.brand=brand;
+    this.model=model;
+    this.color=color;
+    this.price=price;
+}
