dc3daa8db084d69c53584bf84f54af398c494ce0
rahul11nishad <rahul004700@gmail.com>
2022-01-06 20:53:57 +0530

    commiting new changes

--- survey-form.js
+++ survey-form.js
@@ -15,3 +15,14 @@ function Phone(brand,model,color,price)
 Animal.prototype={
     numLegs:4
 }
+
+function Box()
+{
+    let con=document.getElementById("box");
+    this.color=function()
+    {
+        con.setAttribute("background-image","234deg,red,blue");
+    } 
+}
+let kk=new Box();
+kk.color();
